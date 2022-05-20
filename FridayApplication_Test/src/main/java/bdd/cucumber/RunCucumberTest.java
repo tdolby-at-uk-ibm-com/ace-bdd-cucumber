531f30b0dd559e436e29eac00163aee528adaa78
tdolby <tdolby@uk.ibm.com>
2022-05-20 11:14:18 +0100

    Add more tests and actual code

--- FridayApplication_Test/src/main/java/bdd/cucumber/RunCucumberTest.java
+++ FridayApplication_Test/src/main/java/bdd/cucumber/RunCucumberTest.java
@@ -20,21 +20,38 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+// Run the cucumber tests
 @IncludeEngines("cucumber")
 public class RunCucumberTest 
 {
-	// Should probably be using message assemblies instead of strings . . . 
-	static String messageAssemblyName;
+	// Should probably be using message assemblies instead of strings . . .
+	
+	// This is used as one example, using an MXML message file
+	static String messageAssemblyName; 
+	// This is used as another example, providing just the string for the input JSON
+	static String jsonValueForDay;
+	
+	// This is used to hold the result so it can be checked
 	static String returnResult;
 	
+	// Set up for the Sunday test, using an MXML message file
 	@Given("today is Sunday")
 	public void today_is_sunday() {
 		messageAssemblyName = "Sunday";
+		jsonValueForDay = null;
 	}
+	
+	// Set up for the Friday test, using JSON data
+	@Given("today is Friday")
+	public void today_is_friday() {
+		messageAssemblyName = null;
+		jsonValueForDay = "Friday";
+	}
+	
+	// Actually run the flow
 	@When("I ask whether it's Friday yet")
 	public void i_ask_whether_it_s_friday_yet() throws TestException 
 	{
-
 		// Define the SpyObjectReference
 		SpyObjectReference nodeReference = new SpyObjectReference().application("FridayApplication")
 				.messageFlow("MainFlow").node("Compute");
@@ -45,17 +62,26 @@ public class RunCucumberTest
 		// Declare a new TestMessageAssembly object for the message being sent into the node
 		TestMessageAssembly inputMessageAssembly = new TestMessageAssembly();
 
-		// Create a Message Assembly from the input data file
-		try {
-			String messageAssemblyPath = "/"+messageAssemblyName+".mxml";
-			InputStream messageStream = Thread.currentThread().getContextClassLoader()
-					.getResourceAsStream(messageAssemblyPath);
-			if (messageStream == null) {
-				throw new TestException("Unable to locate message assembly file: " + messageAssemblyPath);
+		if ( messageAssemblyName != null )
+		{
+			// Create a Message Assembly from the input data file; can represent any message
+			// tree no matter how complex.
+			try {
+				String messageAssemblyPath = "/"+messageAssemblyName+".mxml";
+				InputStream messageStream = Thread.currentThread().getContextClassLoader()
+						.getResourceAsStream(messageAssemblyPath);
+				if (messageStream == null) {
+					throw new TestException("Unable to locate message assembly file: " + messageAssemblyPath);
+				}
+				inputMessageAssembly.buildFromRecordedMessageAssembly(messageStream);
+			} catch (Exception ex) {
+				throw new TestException("Failed to load input message", ex);
 			}
-			inputMessageAssembly.buildFromRecordedMessageAssembly(messageStream);
-		} catch (Exception ex) {
-			throw new TestException("Failed to load input message", ex);
+		}
+		else
+		{
+			// Directly create the JSON data; useful for small testcases.
+            inputMessageAssembly.messagePath("JSON.Data.today").setValue(jsonValueForDay);
 		}
 
 		// Call the message flow node with the Message Assembly
@@ -70,8 +96,20 @@ public class RunCucumberTest
         /* Compare Output Message 1 at output terminal out */
         TestMessageAssembly actualMessageAssembly = nodeSpy.propagatedMessageAssembly("out", 1);
 
+        // Read the result; this will throw if the value does not exist
         returnResult = actualMessageAssembly.messagePath("JSON.Data.result").getStringValue();	
+
+		// Ensure any mocks created by a test are cleared after the test runs; we could 
+        // leave them around in this case, as the tests all do the same sort of thing, but
+        // clearing everything leaves us with a clean state for every test and avoids 
+        // issues in the future.
+        //
+		// Note that this makes it hard to share message assemblies between steps, and would
+        // need to be changed if message assemblies (as opposed to MXML files) are used.
+		TestSetup.restoreAllMocks();
 	}
+	
+	// Validate the result
 	@Then("I should be told {string}")
 	public void i_should_be_told(String string) 
 	{
@@ -80,8 +118,7 @@ public class RunCucumberTest
 
 	@AfterEach
 	public void cleanupTest() throws TestException {
-		// Ensure any mocks created by a test are cleared after the test runs 
-		// Note that this makes it hard to share message assemblies between steps
+		// Make absolutely sure everything is cleaned up (which it should already be).
 		TestSetup.restoreAllMocks();
 	}
 
