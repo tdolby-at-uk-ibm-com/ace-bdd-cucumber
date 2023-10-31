ea87ac586c615bc9b645681c0b89f70ebb7d127e
Trevor Dolby <trevor.dolby@ibm.com>
2023-10-31 16:43:25 -0500

    Add quay.io image location
    Signed-off-by: Trevor Dolby <trevor.dolby@ibm.com>

--- build-and-run-tests.sh
+++ build-and-run-tests.sh
@@ -16,6 +16,7 @@ echo "================ Building and running FridayApplication tests"
 echo ""
 mqsicreateworkdir /tmp/bdd/FridayApplication-work-dir
 ibmint deploy --input-path $PWD --output-work-directory /tmp/bdd/FridayApplication-work-dir --project FridayApplication --project FridayApplication_Test
+ibmint specify jre --version 11 --work-dir /tmp/bdd/FridayApplication-work-dir
 IntegrationServer -w /tmp/bdd/FridayApplication-work-dir --no-nodejs --start-msgflows no --test-project FridayApplication_Test
 
 echo ""
@@ -23,6 +24,7 @@ echo "================ Building and running WholeFlowApplication tests"
 echo ""
 mqsicreateworkdir /tmp/bdd/WholeFlowApplication-work-dir
 ibmint deploy --input-path $PWD --output-work-directory /tmp/bdd/WholeFlowApplication-work-dir --project WholeFlowApplication --project WholeFlowApplication_Test
+ibmint specify jre --version 11 --work-dir /tmp/bdd/WholeFlowApplication-work-dir
 IntegrationServer -w /tmp/bdd/WholeFlowApplication-work-dir --no-nodejs --start-msgflows no --test-project WholeFlowApplication_Test
 
 echo ""
@@ -31,6 +33,7 @@ echo ""
 # Could run a separate TDD test work directory if there was a danger of interference
 mqsicreateworkdir /tmp/bdd/WholeFlowWithMockedNodeApplication-work-dir
 ibmint deploy --input-path $PWD --output-work-directory /tmp/bdd/WholeFlowWithMockedNodeApplication-work-dir --project WholeFlowWithMockedNodeApplication --project WholeFlowWithMockedNodeApplication_Test --project WholeFlowWithMockedNodeApplication_TDD
+ibmint specify jre --version 11 --work-dir /tmp/bdd/WholeFlowWithMockedNodeApplication-work-dir
 IntegrationServer -w /tmp/bdd/WholeFlowWithMockedNodeApplication-work-dir --no-nodejs --start-msgflows no --test-project WholeFlowWithMockedNodeApplication_TDD
 IntegrationServer -w /tmp/bdd/WholeFlowWithMockedNodeApplication-work-dir --no-nodejs --start-msgflows no --test-project WholeFlowWithMockedNodeApplication_Test
 
